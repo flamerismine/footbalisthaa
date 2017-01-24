45c2cbf6631934da9ccf7759f6f396bd263de27b
Isaac <cemeteries_gater@yahoo.com>
2017-01-24 18:36:49 +0330

    change labels

--- footbalisthaa/frontend/tests/acceptance/HomeCest.php
+++ footbalisthaa/frontend/tests/acceptance/HomeCest.php
@@ -10,7 +10,7 @@ class HomeCest
     public function checkHome(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/index'));
-        $I->see('My Company');
+        $I->see('Footbalisthaa');
         $I->seeLink('About');
         $I->click('About');
         $I->see('This is the About page.');
